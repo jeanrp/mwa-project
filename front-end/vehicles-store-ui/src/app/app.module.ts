92b41cb2b6c0f62ea92e240d526796829b76202b
Gebreslassie Kahsay <ggebretsadic@gmail.com>
2019-06-15 18:41:19 -0500

    Submitting register customer seller form

--- front-end/vehicles-store-ui/src/app/app.module.ts
+++ front-end/vehicles-store-ui/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule, FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import {HttpModule} from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatInputModule } from '@angular/material/input'
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
