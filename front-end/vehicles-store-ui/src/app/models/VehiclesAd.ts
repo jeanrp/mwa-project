5634f583b7c671de3bd9b65b9f832dee70771f30
emmanuell <emmanuellnogmiq@hotmail.com>
2019-06-17 10:52:29 -0500

    Form feed

---
+++ front-end/vehicles-store-ui/src/app/models/VehiclesAd.ts
@@ -0,0 +1,16 @@
+export class VehiclesAd {
+  _id: string;
+  category: String;
+  vin: String;
+  odometer: String;
+  model: String;
+  brand: String;
+  condition: String;
+  fuel: String;
+  color: String;
+  transmission: String;
+  year: String;
+  description: String;
+  price: String;
+  title: String;
+}
