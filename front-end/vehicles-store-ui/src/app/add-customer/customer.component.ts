e5b6e15c7b9fed0496a67332aef75856a7d64064
Gebreslassie Kahsay <ggebretsadic@gmail.com>
2019-06-18 14:25:26 -0500

    Submitting edit and register modifications, adding address to the register customer component

--- front-end/vehicles-store-ui/src/app/add-customer/customer.component.ts
+++ front-end/vehicles-store-ui/src/app/add-customer/customer.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { Router } from "@angular/router";
 import { CustomerSellerService } from "../services/customer-seller.service"
+import { CustomerModel } from '../models/CustomerModel'
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
@@ -10,30 +13,39 @@ import { CustomerSellerService } from "../services/customer-seller.service"
 })
 export class CustomerComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private customerService: CustomerSellerService) { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private customerService: CustomerSellerService, private toastrService : ToastrService) { }
   Repdata;
   valbutton = "Save";
   customerSellerForm: FormGroup;
   submitted = false;
-
+  custotmer: CustomerModel;
+  editForm: FormGroup;
   ngOnInit() {
-    this.customerSellerForm = this.formBuilder.group({
+    this.editForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       phone: ['', Validators.required],
       email: ['', Validators.required],
-      password: ['', Validators.required]
-     
+      password: ['', Validators.required],
+      address: this.formBuilder.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        state: ['', Validators.required],
+        zipcode: ['', Validators.required]
+      })
     });
 
   }
   onSubmit() {
-    this.submitted = true; 
-    if (this.customerSellerForm.valid) {
-      this.customerService.addCustomer(this.customerSellerForm.value)
-        .subscribe(data => {
-          console.log(data);
-          this.router.navigate(['']);
+    this.submitted = true;
+    if (this.editForm.valid) {
+      console.log(this.editForm.value);
+      this.customerService.addCustomer(this.editForm.value).subscribe(data => {
+        this.toastrService.success("You are registered!!!!", "Success");
+        this.router.navigate(['/login']);
+        },
+        error => {
+                console.log(error);
         });
     }
   }
